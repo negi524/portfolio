d59fb288de714de5ea409be59127131babc0ec1d
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-11-19 09:52:37 +0900

    fix: remove ESLint setting

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import eslintPluginAstro from 'eslint-plugin-astro';
 import tsParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-import tailwind from 'eslint-plugin-tailwindcss';
 
 export default [
   // Astro recommended configuration (includes base parser for .astro files)
@@ -10,9 +9,6 @@ export default [
 
   // JSX a11y recommended configuration for accessibility
   jsxA11y.flatConfigs.recommended,
-
-  // Tailwind CSS plugin configuration
-  ...tailwind.configs['flat/recommended'],
 
   // Configure TypeScript parser for script sections within .astro files
   {
